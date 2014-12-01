d558559a5c9de08f9996235702d63092eaf709f8
Olafur Nils Sigurdsson <olafurns10@ru.is>
2014-12-01 15:49:14 +0000

    fourth script

--- sshProduction.sh
+++ sshProduction.sh
@@ -2,21 +2,5 @@
 set -e
 echo "Connecting to production server"
 
-ssh root@178.62.64.58
+ssh root@178.62.64.58 'bash -s' < ssh_script.sh
 
-echo "Stopping and removing all docker containers"
-
-docker kill $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-
-docker ps
-
-echo "Pulling new docker image"
-
-docker pull olafurns/tictactoe
-
-echo "Starting server"
-docker run -p 80:8080 -d -e "NODE_ENV=production" olafurns/tictactoe
-
-"Exiting."
-exit
---
+++ ssh_script.sh
@@ -0,0 +1,17 @@
+
+echo "Stopping and removing all docker containers"
+
+docker kill $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker ps
+
+echo "Pulling new docker image"
+
+docker pull olafurns/tictactoe
+
+echo "Starting server"
+docker run -p 80:8080 -d -e "NODE_ENV=production" olafurns/tictactoe
+
+"Exiting."
+exit
