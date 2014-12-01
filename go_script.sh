1db6fa55758d43d9de35ede14e0fbf1bbaf2da8e
Olafur Nils Sigurdsson <olafurns10@ru.is>
2014-12-01 10:33:40 +0000

    Added pushing to production server to script

--- go_script.sh
+++ go_script.sh
@@ -28,4 +28,23 @@ docker build -t olafurns/tictactoe .
 echo "Pushing docker image"
 docker push olafurns/tictactoe
 
-echo "Done"
+echo "Done with local"
+
+echo "Connecting to production server.."
+
+ssh root@178.62.64.58
+
+echo "Stopping and removing all docker containers"
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+echo "Pulling new docker image"
+
+docker pull olafurns/tictactoe
+
+echo "Starting server"
+docker run -p 80:8080 -d -e "NODE_ENV=production" olafurns/tictactoe
+
+echo "Closing ssh"
+exit
