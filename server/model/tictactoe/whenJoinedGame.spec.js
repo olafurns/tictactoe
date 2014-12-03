3c1871311c8dbac79b9f40ade0c1329b6a997620
Olafur Nils Sigurdsson <olafurns@siminn.is>
2014-12-03 12:17:14 +0000

    first join game test

--- server/model/tictactoe/whenJoinedGame.spec.js
+++ server/model/tictactoe/whenJoinedGame.spec.js
@@ -1,3 +1,52 @@
 /**
  * Created by olafurns on 3.12.2014.
  */
+
+
+var should = require('should');
+var _ = require('lodash');
+var tictactoe = require('./tictactoe.js');
+
+
+describe('join game command', function() {
+
+  it('should emit game joined event', function() {
+
+    var given = [{
+      event: "GameCreated",
+      user: {
+        userName: "Jesus"
+      },
+      name: "RiseOfTheDead",
+      timeStamp: "2014-01-01T01:01:01"
+    }];
+
+    var when = {
+      cmd: "JoinGame",
+      user: {
+        userName: "God"
+      },
+      name: "RiseOfTheDead",
+      timeStamp: "2014-01-01T01:10:01"
+    };
+
+    var then = [{
+      event: "GameJoined",
+      user: {
+        userName: "God"
+      },
+      name: "RiseOfTheDead",
+      timeStamp: "2014-01-01T01:10:01"
+    }];
+
+
+    var actualEvents = tictactoe(given).executeCommand(when);
+    should(actualEvents.length).be.exactly(1);
+
+    should(JSON.stringify(actualEvents)).be.exactly(JSON.stringify(then));
+  });
+
+
+
+})
+
