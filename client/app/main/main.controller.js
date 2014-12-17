e2d4329d77ea9e24d5e87bef1d0418c74e36a0ee
Olafur Nils Sigurdsson <olafurns@siminn.is>
2014-12-17 04:38:10 +0000

    fix

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,8 +4,6 @@ angular.module('tictactoeApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+
 
   });
