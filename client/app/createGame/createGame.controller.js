44b1200eb55e4634a7214834fd6e579d28f2f73c
Olafur Nils Sigurdsson <olafurns@siminn.is>
2014-12-18 21:46:05 +0000

    minor ui

--- client/app/createGame/createGame.controller.js
+++ client/app/createGame/createGame.controller.js
@@ -2,25 +2,28 @@
 /* jshint ignore:start */
 angular.module('tictactoeApp')
   .controller('CreateGameCtrl', function ($scope, $http, guid, $location) {
+
     $scope.createGame = function () {
-      var user = {'userName': $scope.userName, side: 'X'};
 
-      var id = guid();
-      var createPost = $http.post('/api/createGame/', {
-          'id': id,
-          'cmd': 'CreateGame',
-          'user': user,
-          'name': $scope.name,
-          'timeStamp': '2014-12-02T11:29:29'
-        }
-      );
-      createPost.then(function (response) {
-        $location.url('/tictactoe');
-        $location.search('gameId', response.data[0].id);
-        $location.search('gameSide', 'X');
-      });
 
-    };
+
+        var user = {'userName': $scope.userName, side: 'X'};
+
+        var id = guid();
+        $http.post('/api/createGame/', {
+            'id': id,
+            'cmd': 'CreateGame',
+            'user': user,
+            'name': $scope.name,
+            'timeStamp': '2014-12-02T11:29:29'
+          }
+        ).then(function (response) {
+          $location.url('/tictactoe');
+          $location.search('gameId', response.data[0].id);
+          $location.search('gameSide', 'X');
+        });
+
+      };
 
   });
 /* jshint ignore:end */
